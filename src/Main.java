15a3b4431c24308eebf8c4fbd1361ebc5816a0ee
ivasenko <ivasenko98@list.ru>
2023-08-18 13:48:26 +0300

    ready

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(5);
+        binaryTree.add(9);
+        binaryTree.add(8);
+        binaryTree.add(7);
+        binaryTree.add(6);
+        binaryTree.print();
+
+    }
+}
